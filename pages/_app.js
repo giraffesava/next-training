7bbeb1943afb0e0821f777bf7aafd9c1b589bce4
Sava <savely54@mail.ru>
2022-01-14 11:45:55 +0700

    added progress bar library

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import "../styles/main.scss";
+import NextNprogress from "nextjs-progressbar";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <NextNprogress
+        color="darkcyan"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={8}
+        showOnShallow={true}
+      />
       <Component {...pageProps} />
     </>
   );
