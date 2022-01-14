a40a496470870c31d0b328e8517dc251a8a920ab
Sava <savely54@mail.ru>
2022-01-14 11:47:25 +0700

    added request to server

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,12 +1,21 @@
 import Router from "next/router";
-import Head from "next/head";
 import { MainLayout } from "../../layouts/MainLayout";
 
-export default function About() {
+export default function About({ title }) {
   return (
     <MainLayout title="Next">
+      <h1>{title}</h1>
       <h1>About page</h1>
       <button onClick={() => Router.push("/")}>Go back to home</button>
     </MainLayout>
   );
 }
+
+About.getInitialProps = async () => {
+  const response = await fetch("http://localhost:4200/about");
+
+  const data = await response.json();
+  return {
+    title: data.title,
+  };
+};
