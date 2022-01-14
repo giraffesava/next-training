50c468c5a5e8d791aecaca06ef8ae164b66a2669
Sava <savely54@mail.ru>
2022-01-14 11:48:12 +0700

    Checked getServerSideProps working

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { MainLayout } from "../../layouts/MainLayout";
 import { useState, useEffect } from "react";
+import Link from "next/link";
 
 export default function Post({ post: serverPost }) {
   const { query } = useRouter();
@@ -29,10 +30,14 @@ export default function Post({ post: serverPost }) {
     <MainLayout>
       <h1>Post {query.id}</h1>
       <h1>{post.body}</h1>
+      <Link href="/posts">
+        <a>Go Back</a>
+      </Link>
     </MainLayout>
   );
 }
 
+// works on backend and frontend, that's why can check ctx.req
 Post.getInitialProps = async (ctx) => {
   if (!ctx.req) {
     return {
@@ -45,3 +50,17 @@ Post.getInitialProps = async (ctx) => {
     post,
   };
 };
+
+// works only on server
+// export async function getServerSideProps({ query, req }) {
+//   // if (!req) { - can't check it cause doesn't work on frontend
+//   //   return {
+//   //     post: null,
+//   //   };
+//   // }
+//   const response = await fetch(`http://localhost:4200/posts/${ctx.query.id}`);
+//   const post = await response.json();
+//   return {
+//     props: { post },
+//   };
+// }
